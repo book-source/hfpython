e2cb6a88cda5a690ef841b5c026b840697b61d73
Chaniel <Chaniel@users.noreply.github.com>
2015-05-23 16:53:18 +0800

    Update unique.py

--- unique.py
+++ unique.py
@@ -1,7 +1,13 @@
-# 列表项目去重
+# Q: 列表项目去重
 
+
+# A1： 使用循环
 unique_james = []
 
 for each_t in james:
   if each_t not in unique_james:
     unique_james.append(each_t)
+
+# A2： 使用集合set
+
+unique_james = set(james)
